0397f6cd899470314bd621d22871d753cfb286c2
시원 <90040577+SiwonYoo@users.noreply.github.com>
2025-09-11 16:45:05 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
@@ -10,8 +10,8 @@ int solution(vector<int> wallet, vector<int> bill) {
         if (wallet[0] >= bill[0] && wallet[1] >= bill[1]) break;
         if (wallet[1] >= bill[0] && wallet[0] >= bill[1]) break;
     
-        if (bill[0] > bill[1]) bill[0] = bill[0] / 2;
-        else bill[1] = bill[1] / 2;
+        if (bill[0] > bill[1]) bill[0] /= 2;
+        else bill[1] /= 2;
         
         answer++;
     }
