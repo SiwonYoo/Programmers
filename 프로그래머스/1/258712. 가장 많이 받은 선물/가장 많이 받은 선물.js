a61b276142c735471d9b31a441e5a55ca308d0e8
시원 <90040577+SiwonYoo@users.noreply.github.com>
2025-09-15 16:47:36 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 889.20 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,15 +1,15 @@
 // 두 명 비교
-function whoGivenMore(me, friend, gifts) {
-    let myCount = 0;
-    let friendsCount = 0;
+function whoGivenMore(A, B, gifts) {
+    let ACount = 0;
+    let BCount = 0;
     
     for (let item of gifts) {
-        if (item === me + " " + friend) myCount++;
-        else if (item === friend + " " + me) friendsCount++;
+        if (item === A + " " + B) ACount++;
+        else if (item === B + " " + A) BCount++;
     }
     
-    if (myCount > friendsCount) return me;
-    if (friendsCount > myCount) return friend;
+    if (ACount > BCount) return A;
+    if (BCount > ACount) return B;
     return '-1';
 }
 
@@ -17,11 +17,11 @@ function whoGivenMore(me, friend, gifts) {
 function giftScore(person, gifts) {
     let count = 0;
     
-    const givenPerson = gifts.map(item => item.split(' ')[0]);
-    const takenPerson = gifts.map(item => item.split(' ')[1]);
+    const giver = gifts.map(item => item.split(' ')[0]);
+    const taker = gifts.map(item => item.split(' ')[1]);
     
-    givenPerson.forEach(item => {if (item === person) count++;});
-    takenPerson.forEach(item => {if (item === person) count--;})
+    giver.forEach(item => {if (item === person) count++;});
+    taker.forEach(item => {if (item === person) count--;})
       
     return count;
 }
@@ -30,11 +30,10 @@ function solution(friends, gifts) {
     var answer = 0;
     
     // 선물 지수 배열
-    let giftScores = friends.map((item, idx) => giftScore(item, gifts));
+    const giftScores = friends.map((item, idx) => giftScore(item, gifts));
     
     // 받은 선물 배열
-    let giftCount = [];
-    friends.forEach(() => {giftCount.push(0)});
+    const giftCount = new Array(friends.length).fill(0);
     
     for (let i=0; i<friends.length; i++) {
         for (let j=i+1; j<friends.length; j++) {            
