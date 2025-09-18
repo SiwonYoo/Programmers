81bbe7d087d6221af9f8741afb474b0bbd4293f0
시원 <90040577+SiwonYoo@users.noreply.github.com>
2025-09-18 19:21:19 +0900

    [level 1] Title: 공원 산책, Time: 0.61 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,69 +1,53 @@
+let x, y;
+
 function solution(park, routes) {    
-    let x, y;
     for (let i=0; i<park.length; i++) {
-        if (park[i].indexOf('S') !== -1) {
+        if (park[i].includes('S')) {
             [x, y] = [i, park[i].indexOf('S')];
             break;
         }
     }
-    
-    let [tempX, tempY] = [x, y];
-    
+        
     routes.forEach((item) => {
         let [direction, count] = item.split(' ');
         count = Number(count);
-        
-        switch (direction) {
-            case 'E':
-                // 공원을 벗어나는지 확인
-                if (!['O','S'].includes(park[tempX][tempY+count])) break;
-                // 이동 중 장애물을 만나는지 확인
-                for (let i=0; i<count; i++) {
-                    if (!['O','S'].includes(park[tempX][++tempY])) {
-                        tempY = y; // 실패하면 tempY 초기화
-                        break;
-                    }
-                    if (i+1 === count) y = tempY; // 성공하면 y 업데이트
-                }
-                break;
-                
-            case 'S':
-                if (!park[tempX+count] || !['O','S'].includes(park[tempX+count][tempY])) break;
-                for (let i=0; i<count; i++) {
-                    if (!['O','S'].includes(park[++tempX][tempY])) {
-                        tempX = x; 
-                        break;
-                    }
-                    if (i+1 === count) x = tempX;
-                }
-                break;
-                
-            case 'W':
-               if (tempY-count < 0) break;
-               if (!['O','S'].includes(park[tempX][tempY-count])) break;
-
-                for (let i=0; i<count; i++) {
-                    if (!['O','S'].includes(park[tempX][--tempY])) {
-                        tempY = y; 
-                        break;
-                    }
-                    if (i+1 === count) y = tempY;
-                }
-                break;
-                
-            case 'N':
-                if (tempX-count < 0) break;
-                if (!park[tempX-count] || !['O','S'].includes(park[tempX-count][tempY])) break;
-                for (let i=0; i<count; i++) {
-                   if (!['O','S'].includes(park[--tempX][tempY])) {
-                        tempX = x; 
-                        break;
-                    }
-                    if (i+1 === count) x = tempX;
-                }
-                break;
-        }
+       
+        move(direction, count, park);
     })
     
     return [x, y];
+}
+
+function move(direction, count, park) {    
+    let movement = [];
+    
+    if (direction === 'E') movement = [0, 1];
+    else if (direction === 'S') movement = [1, 0];
+    else if (direction === 'W') movement = [0, -1];
+    else if (direction === 'N') movement = [-1, 0];
+    
+    // 공원을 벗어나는지 확인
+    if (!park[x + movement[0]*count]) return;
+    if (!['O','S'].includes(park[x + movement[0]*count][y + movement[1]*count])) return;
+        
+    // 이동 중 장애물을 만나는지 확인
+    let [tempX, tempY] = [x, y];
+
+    for (let i=0; i<count; i++) {
+        tempX += movement[0];
+        tempY += movement[1];
+        
+        if (!['O','S'].includes(park[tempX][tempY])) {
+            // 실패하면 초기화
+            tempX = x;
+            tempY = y; 
+            return;
+        }
+        
+        if (i+1 === count) {
+            // 성공하면 업데이트
+            [x, y] = [tempX, tempY];
+        }
+    }
+    return;
 }
