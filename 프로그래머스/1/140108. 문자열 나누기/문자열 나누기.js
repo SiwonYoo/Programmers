a233d7223b3e4116d58de3e97a6bf8ef96962dff
시원 <90040577+SiwonYoo@users.noreply.github.com>
2025-10-08 23:13:36 +0900

    [level 1] Title: 문자열 나누기, Time: 2.66 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,25 +1,20 @@
-function solution(s) {
-    var answer = 0;
-    
+function solution(s) {    
     let xIdx = 0;
-    let splitArr = [];
+    let score = 0;
     
-    let same = 0;
-    let diff = 0;
-    
+    const splitArr = [];
+  
     [...s].forEach((item, idx) => {
-        if (s[xIdx] === item) same++;
-        else diff++;
+        if (s[xIdx] === item) score++;
+        else score--;
         
-        if (same === diff) {
+        if (!score) {
             splitArr.push(s.slice(xIdx, idx+1));
             xIdx = idx+1;
         } else if (idx === s.length-1) {
             splitArr.push(s.slice(xIdx));
         }
-    })
-    
-    console.log(splitArr);
+    })    
     
     return splitArr.length;
 }
