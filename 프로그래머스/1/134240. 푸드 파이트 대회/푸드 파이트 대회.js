35d343707e5755415c378a768c11441920a02892
시원 <90040577+SiwonYoo@users.noreply.github.com>
2026-01-13 15:59:36 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,11 @@
 function solution(food) {
-    let answer = '';
+    // 왼쪽 사람이 먹을 음식 준비
+    const left = food
+        // item/2씩 준비(나머지는 버림). String의 repeat 메서드 사용.
+        .map((item, idx) => String(idx).repeat(Math.floor(item/2)))
+        // join으로 모든 음식을 문자열로 묶음
+        .join('');
     
-    const sortedFood = [];
-    
-    food.forEach((item, idx) => {
-        if (idx) answer += String(idx).repeat(Math.floor(item/2))
-    })
-    
-    answer = answer + '0' + [...answer].reverse().join('');
-    
-    return answer;
+    // 왼쪽 음식 + 물(0) + 왼쪽을 뒤집어 오른쪽 음식 준비
+    return left + '0' + [...left].reverse().join('');
 }
