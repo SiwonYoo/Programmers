e23038a940b7b8dbbc0dc00e1162782e680f9e92
시원 <90040577+SiwonYoo@users.noreply.github.com>
2026-01-13 15:49:42 +0900

    [level 1] Title: 햄버거 만들기, Time: 230.49 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -6,22 +6,30 @@
 // 3. 보완: 기존 비교 방법은 이 문제에 과하게 의존적임
     // 스택의 마지막 값 기준으로 비교하도록 수정
     // pop 3번 반복 대신 stack.length -= 4; 으로 stack에서 값 제거
-    // 성능은 2보다 안좋아졌지만, 문제에 대한 의존을 줄이고 로직을 단순화시킴
+    // 성능은 2번보다 안좋아졌지만, 문제에 대한 의존을 줄이고 로직을 단순화시킴
     // 메모리: 96.7 MB, 시간: 222.81 ms
 
 function solution(ingredient) {
     var answer = 0;
     
+    // 햄버거를 하나씩 쌓을 스택 준비
     let stack = [];
     
+    // 재료를 하나씩 돌면서
     ingredient.forEach((item, idx, arr) => {
+        // 재료를 하나씩 스택에 쌓음
         stack.push(item);
         
+        // 스택의 길이가 4 이상이 되면
         if (stack.length >= 4) {
+            // 마지막 4개를 잘라 last에 할당
             const last = stack.slice(-4).join('');
             
+            // last가 1231이면
             if (last === '1231') {
+                // 정답 +1
                 answer++;
+                // stack의 length를 조절하여 마지막 4개 값을 제거함
                 stack.length -= 4;
             }
         }
