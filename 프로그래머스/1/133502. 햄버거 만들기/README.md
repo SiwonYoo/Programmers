f509ba4bbbe05455ec342503468bc7c99e2d775f
시원 <90040577+SiwonYoo@users.noreply.github.com>
2026-01-13 15:43:52 +0900

    [level 1] Title: 햄버거 만들기, Time: 222.81 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#) 
 
 ### 성능 요약
 
-메모리: 65.8 MB, 시간: 138.67 ms
+메모리: 96.7 MB, 시간: 222.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 13일 15:21:11
+2026년 01월 13일 15:43:50
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,48 +1,29 @@
+// 이전 시도들
+// 1. join으로 합친 후, includes로 포함 여부 확인 -> replace로 제거 반복
+    // 시간초과
+// 2. stack에 하나씩 넣고 (ingredient[currentIndex] === stack[latestItem] % 3 + 1)로 비교
+    // 성공
+// 3. 보완: 기존 비교 방법은 이 문제에 과하게 의존적임
+    // 스택의 마지막 값 기준으로 비교하도록 수정
+    // pop 3번 반복 대신 stack.length -= 4; 으로 stack에서 값 제거
+
 function solution(ingredient) {
     var answer = 0;
     
-    const stack = [];
+    let stack = [];
     
-//     ingredient.forEach((item, idx, arr) => {
-//         if (isReady(arr, idx)) {
-//             if (item === 3) {
-//                 stack.pop();
-//                 stack.pop();
-//                 stack.pop();
-//             } else {
-//                 stack.push(idx);
-                
-//             }
-//         }
-//     })
-    
-    let idx = 0;
-    
-    while (ingredient) {
-        const item = ingredient[idx];
+    ingredient.forEach((item, idx, arr) => {
+        stack.push(item);
         
-        if (isReady(ingredient, idx)) {
-            if (item === 3) {
-                stack.pop();
-                stack.pop();
-                stack.pop();
-                ingredient.splice(idx-3, 4)
-                idx = stack.pop();
+        if (stack.length >= 4) {
+            const last = stack.slice(-4).join('');
+            
+            if (last === '1231') {
                 answer++;
-                continue;
-            } else {
-                stack.push(idx);
+                stack.length -= 4;
             }
         }
-        
-        idx++;
-    }
+    })
     
     return answer;
-}
-
-function isReady(arr, idx) {
-    if (arr[idx + 1] === 1) return true;
-    if (arr[idx + 1] === arr[idx] % 3 + 1) return true;
-    return false;
 }
