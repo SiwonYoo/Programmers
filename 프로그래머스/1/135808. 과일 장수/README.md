ac5fb51744782448fff244ffec4f0d3260613352
시원 <90040577+SiwonYoo@users.noreply.github.com>
2026-01-13 15:54:34 +0900

    [level 1] Title: 과일 장수, Time: 241.38 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/README.md
+++ 프로그래머스/1/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) | **[⭐️문제 풀이⭐️](./과일 장수.js)**
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
 
 ### 성능 요약
 
-메모리: 94.9 MB, 시간: 302.45 ms
+메모리: 83.5 MB, 시간: 241.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 06일 20:41:15
+2026년 01월 13일 15:54:32
 
 ### 문제 설명
 
--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,21 +1,18 @@
+// 이전 풀이 개선
+// - while 문과 splice를 사용했지만 시간 초과로 다음과 같이 개선했습니다.
+// - for 문과 slice 사용 (원본 배열 수정X)
+// - 최솟값을 구하지 않고 배열의 첫 번째 값을 사용하도록 개선
+
 function solution(k, m, score) {
     let totalCost = 0;
     
     // score 내림차순 정렬
     score.sort((a, b) => b - a);
-    
-    // score의 길이가 m보다 작아질 때까지 계속 반복
-    // while (score.length >= m) {
-    //     // 0부터 m개 잘라낸 배열
-    //     const sliceScore = score.splice(0, m);
-    //     // 잘라낸 배열 중 가장 작은 점수
-    //     const minScore = Math.min(...sliceScore);
-    //     // 가장 작은 점수 * 사과 개수(m)을 totalCost에 추가
-    //     totalCost += minScore * m;
-    // }
-    
-    for (let i=0; score.length - i >= m; i=i+m) {
-        totalCost += Math.min(...score.slice(i, i+m)) * m;
+
+    // i는 m씩 증가 // m+i가 score의 길이보다 크거나 같으면 반복
+    for (let i = 0; score.length >= m+i; i += m) {
+        // i부터 m개 잘라낸 배열의 최솟값 * 개수(m)를 totalCost에 더함
+        totalCost += score[i + m - 1] * m;
     }
     
     return totalCost;
